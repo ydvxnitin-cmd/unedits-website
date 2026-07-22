a5bc7a6de5a5b2c23e56ac461a0fe88f9ec24748
ydvxnitin-cmd <ydvxnitin@gmail.com>
2026-07-22 08:19:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openDrawer(){
+  document.getElementById('drawer').classList.add('open');
+  document.getElementById('drawerOverlay').classList.add('open');
+}
+function closeDrawer(){
+  document.getElementById('drawer').classList.remove('open');
+  document.getElementById('drawerOverlay').classList.remove('open');
+}

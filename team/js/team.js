10e3f13f91c690c324ac95ed2e0c6882bbe38901
ydvxnitin-cmd <ydvxnitin@gmail.com>
2026-08-02 10:26:44 +0530

    Update team.js

--- team/js/team.js
+++ team/js/team.js
@@ -1,1 +1,9 @@
-console.log("UNEDITS Team");
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+alert("Login system will be connected in Phase 2.");
+
+});
